b982318383d948de2db4c2689d0693472c4fc2fb
SantiagoCorales <144611846+SantiagoCorales@users.noreply.github.com>
2025-10-17 23:29:56 +0000

    [Generador de excusas pre work]
    - Modifique el erro de mi index.html.
    - Mejore la funcion ed mi app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function RandomElement(array) {
+  return array [Math.floor(Math.random() * array.length)];
+}
+
+function generateExcuse() {
+let who = ['El vecino', 'Mi perro', 'Un vagabundo', 'El presidente', 'Mi gato'];
+let action = ['rompio', 'se comio', 'quemo', 'partio'];
+let what = ['mi auto', 'el internet', 'mi proyecto final', 'la tesis', 'mi celular'];
+let when = ['cuando estaba regando las plantas', 'cuando saque a pasear a mi marciano', 'mientras hacia yoga con Dalai Lama', 'luego de que fuera a la tienda por los ungüentos de la abuela', 'mientras saltaba en mi trampolin', 'mientras hacia un viaje a Marte', 'cuando estaba en el Capitolio'];
+
+let excuse = `${RandomElement(who)} ${RandomElement(action)} ${RandomElement(what)} ${RandomElement(when)}`;
+
+document.getElementById("excuse").innerText = excuse;
+}
+window.onload = generateExcuse
